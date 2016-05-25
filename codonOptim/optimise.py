c475dfe7043f3f30a3a8ab6c4e355dee1bf4893e
Haydn King <hjking734@gmail.com>
2016-05-25 15:52:58 +0100

    Basic optimise script

--- codonOptim/optimise.py
+++ codonOptim/optimise.py
@@ -4,36 +4,100 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 import Bio.Alphabet as Alphabet
 
+import argparse, os.path
+
+def get_arguments():
+	parser = argparse.ArgumentParser(description="Perform codon optimisation")
+	parser.add_argument("genome", help="Genome file to build codon table from")
+	parser.add_argument("gene_sequence", nargs='+', help="Sequence file(s) to optimise")
+	parser.add_argument("-s", "--scheme", 
+											nargs=1, 
+											required=False, 
+											default="simple",
+											choices=["simple",],
+											help="Which optimisation scheme to use. Valid option are "+
+											  "\'simple\' (default): replace each codon with a "+
+												"randomised codon with probability equal to background"+
+												"(subject to --ignore-rare)"
+											)
+	parser.add_argument("-r", "--ignore-rare", 
+											required=False,
+											type=float,
+											default=0.,
+											help="Do not include codons with frequency below given "+
+												"percentage in output. "+
+												"For example \'AUU\', \'AUC\', and \'AUA\' make up "+
+												"49%, 37%, and 13% of Isoleucine codons in Bacillus "+
+												"Subtilis; setting --ignore-rare=15 will discard "+
+												"\'AUA\' and emit \'AUU\' and \'AUC\' with "+
+												"probabilities 0.57 and 0.43")
+#	parser.add_argument("--UTR",
+#											nargs=1,
+#											required=False,
+#											help="Sequence file providing the 5'UTR which will be "+
+#											"used for the optimised gene. Secondary structure "+
+#											"in the bases around the RBS (estimated as [-19,+12] "+
+#											"from the start codon) will be minimised while "+
+#											"respecting --ignore-rare if given")
+
+	args = parser.parse_args()
+
+	if args.ignore_rare:
+		if args.ignore_rare > 100 or args.ignore_rare < 0:
+			raise ValueError("--ignore-rare must be within [0,100] not {}".format(args.ignore_rare))
+
+	return args
+
+def load_sequence(filename):
+
+	try:
+		return SeqIO.read(filename, "fasta")
+	except ValueError:
+		pass
+
+	try:
+		return SeqIO.read(filename, "genbank")
+	except ValueError:
+		pass
+	
+	raise ValueError("Could not load file \'{}\'.".format(filename))
 
 def main():
 
-	marchantia = SeqIO.read("./data/m.polymorpha.gb", "genbank")
+	args = get_arguments()
 
-	b = bias.Bias(marchantia)
+	genome = load_sequence(args.genome)
+
+	b = bias.Bias(genome)
 
 	print(b.stats())
 	print(b)
 
-	test = SeqIO.read("./data/efor-red.fasta", "fasta")
-	UTR = SeqIO.read("./data/UTR.fasta", "fasta")
 
-	out = codon_optimise(b, test, 0.1, UTR)
+	for filename in args.gene_sequence:
+		seq = load_sequence(filename)
+		if args.scheme == "simple":
+			out = codon_optimise(b, seq, args.ignore_rare/100.)
 
-	SeqIO.write(out, "./data/efor-red-optim.fasta", "fasta")
+		SeqIO.write(out, os.path.splitext(filename)[0] + ".optim.fasta", "fasta")
 
+def translate(seq):
+	return [bias.inv_codon_table[str(seq[i:i+3]).upper()] for i in range(0, len(seq), 3)]
 
-def codon_optimise(b, sr, rare_codon_cutoff=0.1, UTR=None):
+def codon_optimise(b, sr, rare_codon_cutoff=0.1):
 
+	AA = translate(str(sr.seq))
 	oseq = []
-	for icdn in (sr.seq[i:i+3].upper() for i in range(0, len(sr), 3)):
-		oseq.append(b.emit(bias.inv_codon_table[icdn], rare_codon_cutoff))
+	for aa in AA:
+		oseq.append(b.emit(aa, rare_codon_cutoff))
+	oseq = ''.join(oseq)
 
-	if UTR:
-		start = str(UTR.seq)[-22:] + ''.join(oseq)[:16]
-		print(start)
-		print(rnafold.fold(start))
+	oAA = translate(oseq) 
+	if AA != oAA:
+		raise ValueError("Translations don't match")
 
-	return SeqRecord(Seq(''.join(oseq), Alphabet.generic_dna),
+
+	return SeqRecord(Seq(oseq, Alphabet.generic_dna),
 									 id = sr.id,
 									 name=sr.name,
 									 description=sr.description)
