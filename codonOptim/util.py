a1fb7d45e38c0db9dbbbc6bae0ec8f55294a5dd1
Haydn King <hjking734@gmail.com>
2016-06-07 15:06:02 +0100

    Add PCA.py script

--- codonOptim/util.py
+++ codonOptim/util.py
@@ -13,3 +13,16 @@ def load_sequence(filename):
 		pass
 	
 	raise ValueError("Could not load file \'{}\'.".format(filename))
+
+def check_folder_exists(name):
+	if not os.path.isdir(name):
+		resp = ''
+		while resp.upper() not in ['Y','N',]:
+			resp = raw_input(("Output directory \'{}\' doesn't exist. "+
+												"Create it? [Y/N]: ").format(args.output_folder))
+		if resp.upper() == 'Y':
+			os.mkdir(args.output_folder)
+		else:
+			return False
+	return True
+
